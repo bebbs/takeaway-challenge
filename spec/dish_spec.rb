288aa99fbe970379d9643f1ca18a7678bf3d1219
Josh Bebbington <josh@focalwebsites.com>
2014-12-23 19:41:46 +0000

    Added further test coverage to dish_spec

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -2,7 +2,11 @@ require 'dish'
 
 describe Dish do
 
-  let(:pizza){Dish.new(:pizza, 7)}
+  let(:pizza){Dish.new('pizza', 7)}
+
+  it 'should be created with a name' do
+    expect(pizza.name).to eq 'pizza'
+  end
 
   it 'should be created with a price' do
     expect(pizza.price).to eq 7
