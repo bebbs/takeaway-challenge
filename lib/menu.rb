ac7a98f7197e44c53b5d4fcd5bfeec7af01187a9
Josh Bebbington <josh@focalwebsites.com>
2014-12-30 13:15:32 +0000

    Added verification and fetching of dishes within a menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -8,8 +8,20 @@ class Menu
 
   attr_reader :name, :dish, :dishes
 
-  def add(name, price)
-    dishes << (dish[name.to_sym] = price)
+  def add(dish)
+    dishes << dish
+  end
+
+  def dish_count
+    dishes.length
+  end
+
+  def exists?(dish)
+    dishes.any? { |d| d.name == dish }
+  end
+
+  def fetch_by_name(dish)
+    dishes.select { |d| d.name == dish}.first
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,16 +3,47 @@ require 'menu'
 describe Menu do
   
   let(:menu){Menu.new("Main")}
-  it 'should have a name' do
-    expect(menu.name).to eq "Main"
+  let(:dish1){ double :dish, name: "Korma"}
+  let(:dish2){ double :dish, name: "Jalfrezi"}
+
+  before(:each){menu.add(dish1)}
+
+  context 'basic dish menu' do
+
+    it 'should have a name' do
+      expect(menu.name).to eq "Main"
+    end
+
+    it 'should have a list of dishes' do
+      expect{menu.add(dish2)}.to change{menu.dishes.length}.by 1
+    end
+
+    it 'should have a count of dishes' do
+      expect(menu.dish_count).to eq 1
+    end
+
   end
 
-  it 'should have a list of dishes' do
-    menu.add("Korma", 9.00)
-    expect(menu.dishes.length).to eq 1
+  context 'verifying dishes' do
+
+    it 'should know if a dish exists' do
+      expect(menu.exists?("Korma")).to be true
+    end 
+
+    it 'should know if a dish doesn\'t exist' do
+      expect(menu.exists?("Lamb Bhuna")).to be false
+    end
+
   end
 
-  it 'should count its dishes' do
+  context 'finding dishes' do
+
+    it 'should be able to find a dish object by its name' do
+      expect(menu.fetch_by_name("Korma")).to eq dish1
+    end
+
   end
 
 end
+
+
